9d01f906e3e0122486ea743fee78a0c036c53e05
Michias Shiferaw <mshif073@uottawa.ca>
2022-08-17 12:00:48 -0400

    created gulpfile for sass

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
        <p>Prep for Github Website</p>
+       <h1><a>Hello</a></h1>
+       <h1>This is an h1</h1>
+       <a href='#/'>Anchor tag</a>
+       <p className='error'>Error paragraph</p>
+       <p className='notification'>this is notification</p>
       </header>
     </div>
   );
