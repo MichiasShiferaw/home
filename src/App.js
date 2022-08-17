b7103edd7a14fdce87b8906fa09d4fc0b83349c4
Michias Shiferaw <mshif073@uottawa.ca>
2022-08-17 14:26:20 -0400

    Implemented colors scss file

--- src/App.js
+++ src/App.js
@@ -7,11 +7,67 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
        <p>Prep for Github Website</p>
-       <h1><a>Hello</a></h1>
-       <h1>This is an h1</h1>
-       <a href='#/'>Anchor tag</a>
-       <p className='error'>Error paragraph</p>
-       <p className='notification'>this is notification</p>
+
+
+{/* colors */}
+
+<h2>Colors</h2>
+    <span class="text-primary">primary text</span> | 
+    <span class="text-secondary">secondary text</span> | 
+    <span class="text-error">error text</span> | 
+    <span class="text-info">info text</span> | 
+    <span class="text-blue">blue text</span> | 
+    <span class="text-red">red text</span> |
+    <span class="text-green">green text</span> | 
+    <span class="text-yellow">yellow text</span> | 
+    <span class="text-purple">purple text</span> | 
+    <span class="text-orange">orange text</span> | 
+    <span class="text-gray">gray text</span>
+
+    <br/>
+
+    <span class="bg-primary text-white">bg primary</span> |
+    <span class="bg-secondary text-white">bg secondary</span> |
+    <span class="bg-error text-white">bg error</span> |
+    <span class="bg-info text-white">bg info</span> |
+    <span class="bg-blue text-white">bg blue</span> |
+    <span class="bg-red text-white">bg red</span> |
+    <span class="bg-green text-white">bg green</span> |
+    <span class="bg-yellow text-white">bg yellow</span> |
+    <span class="bg-purple text-white">bg purple</span> |
+    <span class="bg-orange text-white">bg orange</span> |
+    <span class="bg-gray text-white">bg gray</span> |
+
+    <br/>
+
+    <span class="bg-primary-dark-8 text-white">primary dark 8</span> |
+    <span class="bg-primary-dark-6 text-white">primary dark 6</span> |
+    <span class="bg-primary-dark-4 text-white">primary dark 4</span> |
+    <span class="bg-primary-dark-2 text-white">primary dark 2</span> |
+    <span class="bg-primary text-white">primary</span> |
+    <span class="bg-primary-light-2 text-white">primary light 2</span> |
+    <span class="bg-primary-light-4 text-white">primary light 4</span> |
+    <span class="bg-primary-light-6 text-white">primary light 6</span> |
+    <span class="bg-primary-light-8 text-white">primary light 8</span> |
+
+
+
+{/* font sizes */}
+
+{/* buttons */}
+
+{/* cards */}
+<h2>Card</h2>
+<div className="card">
+  <h1 className='card-title'>This is a card title</h1>
+  <p className="card-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec faucibus pretium vehicula. In odio lacus, posuere venenatis urna in, feugiat vehicula urna. Nullam feugiat augue non elit dapibus, vel bibendum elit sagittis. Pellentesque sagittis lacus sed nisi feugiat congue. In in cursus leo. Nam vel rhoncus turpis. Duis in neque rutrum, euismod nisl ut, eleifend erat. </p>
+</div>
+
+{/* grid system */}
+
+{/* utilities */}
+
+
       </header>
     </div>
   );
