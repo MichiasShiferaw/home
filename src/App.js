6c18d7dcd7c989cdada6b84dc9ad2e7409fd0aa4
Michias Shiferaw <mshif073@uottawa.ca>
2022-08-22 19:46:40 -0400

    Updates to _base.scss files and added icons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 import Modal from './Components/Modal/modal';
 import Sidebar from './Components/Sidebar1/Sidebar';
+import { ProfileInfo } from './Components/Sidebar1/SidebarData';
+import logo from './avatar.svg';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
        <p>Prep for Github Website</p> */}
 
-  <nav className="navbar justify-between">
+  <nav className="navbar justify-between bg-gray">
     <div className="container">
       <h1 className="site-title">Michias Shiferaw</h1>
       <ul className="display-f">
@@ -19,7 +20,7 @@ function App() {
         <li className="ml-1 text-hover-secondary"><a href="#contact">Contact</a></li>
       </ul>
     </div>
-  </nav>
+  </nav> 
 <Sidebar>
 {/* colors */}
  {/* <!-- navbar --> */}
@@ -31,15 +32,17 @@ function App() {
   <div className="container mt-5">
     <div className="row justify-center">
       <div className="col-12-xs col-5-md">
+        <p className="text-gray mb-2">Hi</p>
         <h2>
-          <div className="font-xxl">Black-Belt</div>
-          <div className="font-xxl text-secondary">Your Website</div>
+          
+          <div className="font-xxl">Michias Shiferaw</div>
+          <div className="font-xl text-secondary">Developer</div>
         </h2>
-        <p className="text-gray mt-2 mb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-        <a href="#work" className="btn-outlined-secondary text-secondary text-hover-white">View Our Work</a>
+        <p className="text-gray mt-2 mb-3">Welcome to my Website!</p>
+        <a href="#work" className="btn-outlined-secondary text-secondary text-hover-white">View My Work</a>
       </div>
       <div className="col-12-xs col-5-md">
-        <img src="/img/laptop.svg" alt=""/>
+        <img src={logo} alt=""/>
       </div>
     </div>
   </div>
@@ -49,52 +52,219 @@ function App() {
     <div className="container">
       <h2 className="mb-2">About Me!</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa ipsam animi aliquid sequi fuga, nam nesciunt dolore libero dolorem exercitationem aliquam cupiditate atque illo, quae dicta doloribus et? Ab ipsam inventore quam asperiores, sequi unde tenetur accusamus, distinctio magni necessitatibus quis deserunt id alias. Iste eum ea labore rerum voluptatibus.</p>
-      <p className="mt-1">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta nam, corrupti dolorum inventore perspiciatis id illum repellat iste amet sapiente ducimus nihil molestias quasi, totam, ratione minima molestiae blanditiis iure consequatur praesentium debitis. Nulla maiores doloremque tempore nobis dolorum amet!</p>
+      {/* <p className="mt-1">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta nam, corrupti dolorum inventore perspiciatis id illum repellat iste amet sapiente ducimus nihil molestias quasi, totam, ratione minima molestiae blanditiis iure consequatur praesentium debitis. Nulla maiores doloremque tempore nobis dolorum amet!</p> */}
     </div>
   </section>
 
   {/* <!-- work section --> */}
   <section id="work" className="mt-5">
     <div className="container">
-      <h2 className="mb-2">Some of Our Work</h2>
+      <h2 className="mb-2">My Work</h2>
       <div className="row gap-2">
-        <div className="col-12-xs col-6-md col-3-lg">
-          <div className="card p-0">
-            <h3 className="card-title m-1">
-              Mario Club <span className="badge-orange text-white ml-1">new</span>
+        <div className="col-12-xs col-6-md col-4-lg">
+          <div className="card h-100 p-0">
+            <h3 className="card-title m-1 mb-0">
+              Project 1 
+            </h3>
+            <span className="badge-orange text-white ml-1">new</span>
+            <img src="/img/mario.png" alt=""/>
+            <p className="m-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, hic!</p>
+          </div>
+        </div>
+        <div className="col-12-xs col-6-md col-4-lg">
+          <div className="card h-100 p-0">
+            <h3 className="card-title m-1">Project 2</h3>
+            <img src="/img/food.png" alt=""/>
+            <p className="m-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, hic!</p>
+          </div>
+        </div>
+        <div className="col-12-xs col-6-md col-4-lg">
+          <div className="card h-100 p-0">
+            <h3 className="card-title m-1">Project 3</h3>
+            <img src="/img/marmite.png" alt=""/>
+            <p className="m-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, hic!</p>
+          </div>
+        </div>
+        <div className="col-12-xs col-6-md col-4-lg">
+          <div className="card h-100 p-0">
+            <h3 className="card-title m-1">Project 4</h3>
+            <img src="/img/notes.png" alt=""/>
+            <p className="m-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, hic!</p>
+          </div>
+        </div>
+        <div className="col-12-xs col-6-md col-4-lg">
+          <div className="card h-100 p-0">
+            <h3 className="card-title m-1 mb-0">
+              Project 5 
             </h3>
             <img src="/img/mario.png" alt=""/>
             <p className="m-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, hic!</p>
           </div>
         </div>
-        <div className="col-12-xs col-6-md col-3-lg">
-          <div className="card p-0">
-            <h3 className="card-title m-1">Ninja Food</h3>
+        <div className="col-12-xs col-6-md col-4-lg">
+          <div className="card h-100 p-0">
+            <h3 className="card-title m-1">Project 6</h3>
             <img src="/img/food.png" alt=""/>
             <p className="m-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, hic!</p>
+            <ul className="display-f list-unstyled mt-auto">
+              <li className="me-auto">
+                              <img src={'./tools/bootstrap1.png'} alt="Bootstrap" width="32" height="32" title='Bootstrap 5'/>
+                <img src={'./tools/react.png'} alt="ReactJs" width="32" height="32" title='ReactJS'/>
+                <img src={'./tools/firebase.png'} alt="ReactJs" width="32" height="32"/>
+              </li>
+              <li className="d-flex align-items-center me-3">
+                <small>See More</small>
+              </li>
+            </ul>
+          
           </div>
         </div>
-        <div className="col-12-xs col-6-md col-3-lg">
-          <div className="card p-0">
-            <h3 className="card-title m-1">Just Add Marmite</h3>
+        <div className="col-12-xs col-6-md col-4-lg">
+          <div className="card h-100 p-0">
+            <h3 className="card-title m-1">Project 7</h3>
             <img src="/img/marmite.png" alt=""/>
-            <p className="m-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, hic!</p>
+            <p className="m-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, hicgdergsdfsdf s wer!</p>
           </div>
         </div>
-        <div className="col-12-xs col-6-md col-3-lg">
-          <div className="card p-0">
-            <h3 className="card-title m-1">Ninja Notes</h3>
+        <div className="col-12-xs col-6-md col-4-lg">
+          <div className="card h-100 p-0">
+            <h3 className="card-title m-1">Project 8</h3>
             <img src="/img/notes.png" alt=""/>
             <p className="m-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, hic!</p>
           </div>
         </div>
       </div>
-      <div className="row justify-center mt-2">
+      {/* <div className="row justify-center mt-2">
         <button className="btn-secondary text-white font-md">View All</button>
-      </div>
+      </div> */}
     </div>
   </section>
 
+
+  <section id="school" className="mt-5">
+    <div className="container text-dark">
+              <div className="row pb-5">
+                <div className="col-md-12 col-lg-6">
+                  <h1 className="text-white pb-2">Education</h1>
+                  <div className="educate rounded shadow-dark text-start overflow-hidden">
+                    <div className="card education verticalLine d-flex mb-3 w-100 p-3 pb-1 border-bottom" data-aos="flip-down">
+                      <div className="text-primary d-flex justify-content-center px-2">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="40"
+                          height="40"
+                          fill="currentColor"
+                          className="bi bi-mortarboard"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M8.211 2.047a.5.5 0 0 0-.422 0l-7.5 3.5a.5.5 0 0 0 .025.917l7.5 3a.5.5 0 0 0 .372 0L14 7.14V13a1 1 0 0 0-1 1v2h3v-2a1 1 0 0 0-1-1V6.739l.686-.275a.5.5 0 0 0 .025-.917l-7.5-3.5ZM8 8.46 1.758 5.965 8 3.052l6.242 2.913L8 8.46Z" />
+                          <path d="M4.176 9.032a.5.5 0 0 0-.656.327l-.5 1.7a.5.5 0 0 0 .294.605l4.5 1.8a.5.5 0 0 0 .372 0l4.5-1.8a.5.5 0 0 0 .294-.605l-.5-1.7a.5.5 0 0 0-.656-.327L8 10.466 4.176 9.032Zm-.068 1.873.22-.748 3.496 1.311a.5.5 0 0 0 .352 0l3.496-1.311.22.748L8 12.46l-3.892-1.556Z" />
+                        </svg>
+                      </div>
+                      <div className="content mb-5 ps-3">
+                        <span className="time">2020-Present</span>
+                        <h3 className="title"> Software Engineering</h3>
+                        <h4 className="lead"> Mother Tersea</h4>
+                        <span>
+                          <i className="bi bi-geo-alt me-2"></i>Ontario, Canada
+                        </span>
+                        <p>
+                          <strong>Revevant Coursework</strong>: Object Oriented
+                          Programming, Data Structures and Algorithms,
+                          Introduction to Software Engineerin
+                        </p>
+                      </div>
+                    </div>
+                    <div className="card education verticalLine d-flex mb-3 w-100 p-3 pb-1 border-bottom" data-aos="flip-down">
+                      <div className="text-primary d-flex justify-content-center px-2">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="40"
+                          height="40"
+                          fill="currentColor"
+                          className="bi bi-mortarboard"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M8.211 2.047a.5.5 0 0 0-.422 0l-7.5 3.5a.5.5 0 0 0 .025.917l7.5 3a.5.5 0 0 0 .372 0L14 7.14V13a1 1 0 0 0-1 1v2h3v-2a1 1 0 0 0-1-1V6.739l.686-.275a.5.5 0 0 0 .025-.917l-7.5-3.5ZM8 8.46 1.758 5.965 8 3.052l6.242 2.913L8 8.46Z" />
+                          <path d="M4.176 9.032a.5.5 0 0 0-.656.327l-.5 1.7a.5.5 0 0 0 .294.605l4.5 1.8a.5.5 0 0 0 .372 0l4.5-1.8a.5.5 0 0 0 .294-.605l-.5-1.7a.5.5 0 0 0-.656-.327L8 10.466 4.176 9.032Zm-.068 1.873.22-.748 3.496 1.311a.5.5 0 0 0 .352 0l3.496-1.311.22.748L8 12.46l-3.892-1.556Z" />
+                        </svg>
+                        <span className="line"></span>
+                      </div>
+                      <div className="content mb-5 ps-3">
+                        <span className="time">2020-2021</span>
+                        <h3 className="title"> Software Engineering</h3>
+                        <h4 className="lead"> Code Academy</h4>
+                        <span>
+                          <i className="bi bi-geo-alt me-2"></i>Ontario, Canada
+                        </span>
+                        <p>
+                          Focuses in: JavaScript, JSON, HTML/CSS, Ruby, Ruby on
+                          Rails, MySQL, PostgreSQL, GraphQL
+                        </p>
+                      </div>
+                    </div>
+                    <div className="card education verticalLine d-flex mb-3 w-100 p-3 pb-1 border-bottom" data-aos="flip-down">
+                      <div className="text-primary d-flex justify-content-center px-2">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="40"
+                          height="40"
+                          fill="currentColor"
+                          className="bi bi-mortarboard"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M8.211 2.047a.5.5 0 0 0-.422 0l-7.5 3.5a.5.5 0 0 0 .025.917l7.5 3a.5.5 0 0 0 .372 0L14 7.14V13a1 1 0 0 0-1 1v2h3v-2a1 1 0 0 0-1-1V6.739l.686-.275a.5.5 0 0 0 .025-.917l-7.5-3.5ZM8 8.46 1.758 5.965 8 3.052l6.242 2.913L8 8.46Z" />
+                          <path d="M4.176 9.032a.5.5 0 0 0-.656.327l-.5 1.7a.5.5 0 0 0 .294.605l4.5 1.8a.5.5 0 0 0 .372 0l4.5-1.8a.5.5 0 0 0 .294-.605l-.5-1.7a.5.5 0 0 0-.656-.327L8 10.466 4.176 9.032Zm-.068 1.873.22-.748 3.496 1.311a.5.5 0 0 0 .352 0l3.496-1.311.22.748L8 12.46l-3.892-1.556Z" />
+                        </svg>
+                      </div>
+                      <div className="content mb-5 ps-3">
+                        <span className="time">2019-2020</span>
+                        <h3 className="title"> High School</h3>
+                        <h4 className="lead"> Mother Tersea</h4>
+                        <span>
+                          <i className="bi bi-geo-alt me-2"></i>Ontario, Canada
+                        </span>
+                        <p>
+                          <strong>Revevant Coursework</strong>: Object Oriented
+                          Programming, Data Structures and Algorithms,
+                          Introduction to Software Engineerin
+                        </p>
+                      </div>
+                    </div>
+                    <div className="vr"></div>
+                  </div>
+                  {/* </div> */}
+                </div>
+                </div>
+                </div>
+  </section>
+
+  <footer className="footer bg-gray-light-7 justify-between pt-2 pb-3 mt-4">
+    
+    <div className="container mt-2">
+      
+      {/* copyright 2022 Michias Shiferaw */}
+      <div><span className='site-title'>Logo  </span><span className='site-title'>Michias Shiferaw</span>
+      </div>
+      <ul className="display-f">
+        {ProfileInfo.map((val, key) => {
+        return (
+          <li
+            className="ml-1 text-hover-secondary"
+            key={key}
+          >
+            <a className="btn p-0"href={val.link}rel="noreferrer" target="_blank">
+            <div id="icon" title={val.title}>{val.icon}</div>
+            </a>
+          </li>
+        );
+      })}
+      </ul>
+    </div>
+  </footer>
+
+<div className='mt-5'>Done! Ignore the Rest</div>
+<hr className='bg-black mb-5'/>
 
   <div className="container">
 
@@ -224,9 +394,27 @@ function App() {
 
 </div>
   {/* <!-- footer --> */}
-  <footer className="bg-gray-light-7 pt-3 pb-3 mt-5">
-    <div className="container">
-      copyright 2022 Michias Shiferaw
+  <footer className="footer bg-gray-light-7 justify-between pt-2 pb-3 mt-4">
+    
+    <div className="container mt-2">
+      
+      {/* copyright 2022 Michias Shiferaw */}
+      <div><span className='site-title'>Logo  </span><span className='site-title'>Michias Shiferaw</span>
+      </div>
+      <ul className="display-f">
+        {ProfileInfo.map((val, key) => {
+        return (
+          <li
+            className="ml-1 text-hover-secondary"
+            key={key}
+          >
+            <a className="btn p-0"href={val.link}rel="noreferrer" target="_blank">
+            <div id="icon" title={val.title}>{val.icon}</div>
+            </a>
+          </li>
+        );
+      })}
+      </ul>
     </div>
   </footer>
 
--- src/Components/Sidebar1/Sidebar.js
+++ src/Components/Sidebar1/Sidebar.js
@@ -1,39 +1,86 @@
 import React, { useState } from 'react';
-import { SidebarData } from './SidebarData';
-import logo from '../../avatar-2.svg';
+import { SidebarData,ProfileInfo } from './SidebarData';
+import logo from '../../avatar.svg';
 
 
 const Sidebar=({children})=> {
     const[isOpen ,setIsOpen] = useState(false);
     const toggle = () => setIsOpen (!isOpen);
   return (
-    <div style={{display:"flex", textAlign:"center"}}>
-    <div style={{width: isOpen ? "250px" : "50px"}} className='Sidebar'>
-        <div className='topSection'>
-            <h1 style={{display:isOpen?"block":"none"}} className="logo">Michias</h1>
-            <div style={{marginLeft: isOpen ? "80px" : "0px"}} className="bars">
-                       <button className="nav_btn"  onClick={toggle}>{isOpen?(<i class="bi bi-arrow-left-circle"></i>):(<i class="bi bi-arrow-right-circle"></i>)}</button>
-                   </div>
-                 <hr className="bg-danger border-2 border-top border-danger"/>
-                 {/* <hr /> */}
-                   
-        </div>
-        <hr />
-        <div>
-        <img className="logo1 pt-2 pb-1"src={logo}/>
-        <div style={{display:isOpen?"block":"none",textDecoration: "underline"}} class="h4 name pt-1 mb-2 text-wrap">Michias Shiferaw</div>
-        </div>
-        <ul className='SidebarList pt-5'>{SidebarData.map((val,key)=>{
-        return(
-            <li className="row" key={key} id={window.location.pathname== val.link? "active":""} onClick={()=>{window.location.pathname=val.link}}>
-                <div id="icon">{val.icon}</div>
-                <div  style={{display: isOpen ? "block" : "none"}} id="title">{val.title}</div>
-            </li>
-
-        )
-    })}</ul></div>
-    <main className='bg-gray'>{children}</main>
+<div style={{ display: "flex", textAlign: "center" }}>
+  <div style={{ width: isOpen ? "250px" : "50px" }} className="Sidebar">
+    <div className="topSection">
+      <h1 style={{ display: isOpen ? "block" : "none" }} className="logo">
+        Logo
+      </h1>
+      <div style={{ marginLeft: isOpen ? "80px" : "-10px" }} className="bars">
+        <button className="btn nav_close p-0" onClick={toggle}>
+          {isOpen ? (
+            <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-arrow-left-circle" viewBox="0 0 16 16">
+  <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
+</svg>
+          ) : (
+            <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+  <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
+</svg>
+          )}
+        </button>
+      </div>
+      <hr className="bg-danger border-2 border-top border-danger" />
+      {/* <hr /> */}
     </div>
+    <hr />
+    <div>
+      <img className="logo1 pt-2 pb-1" src={logo} />
+      <h2
+        style={{
+          display: isOpen ? "block" : "none",
+          // textDecoration: "underline",
+        }}
+        className="h2 name pt-1 mb-2 text-wrap text-white"
+      >
+        Michias Shiferaw
+      </h2>
+    </div>
+    <ul className="SidebarList">
+      {SidebarData.map((val, key) => {
+        return (
+          <li
+            className="row font-md"
+            key={key}
+            id={window.location.pathname == val.link ? "active" : ""}
+            onClick={() => {
+              window.location.pathname = val.link;
+            }}
+          >
+            <div id="icon">{val.icon}</div>
+            <div style={{ display: isOpen ? "block" : "none" }} id="title">
+              {val.title}
+            </div>
+          </li>
+          
+        );
+      })}
+    </ul>
+
+        <ul style={{ display: isOpen ? "flex" : "block" }} className='myLogo pt-3'>
+      {ProfileInfo.map((val, key) => {
+        return (
+          <li
+            className="row line"
+            key={key}
+          >
+            <a className="btn p-0"href={val.link}rel="noreferrer" target="_blank">
+            <div id="icon" title={val.title}>{val.icon}</div>
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+  </div>
+  <main>{children}</main>
+</div>
+
   )
 }
 
