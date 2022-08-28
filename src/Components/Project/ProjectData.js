a737c6c39f403523f9b0b9f809c30097514fce51
Michias Shiferaw <mshif073@uottawa.ca>
2022-08-28 11:30:39 -0400

    Updated  dark/light mode

--- src/Components/Project/ProjectData.js
+++ src/Components/Project/ProjectData.js
@@ -1,51 +1,32 @@
-export const ProjectData=[
-    {
-        title:"Animated Website",
-        tools:[
-            "Sass","React","Firebase"
-        ],
-        link:"/home"
-
-    },
-    {
-        title:"Personal Website",
-        tools:[
-            "Bootstrap","React","Firebase"
-        ],
-        link:"/home"
-
-    },
-    {
-        title:"Face Your Food",
-        tools:[
-            "Bootstrap","React","Firebase"
-        ],
-        link:"/home"
-
-    },
-     {
-        title:"CarDoctor",
-        tools:[
-            "Bootstrap","React"
-        ],
-        link:"/project"
-
-    },
-        {
-        title:"Animated Game",
-        tools:[
-            "Vanillajs"
-        ],
-        link:"/code"
-
-    },
-            {
-        title:"Costco Management",
-        tools:[
-            "C++"
-        ],
-        link:"/code"
-
-    },
-
-]
+export const ProjectData = [
+  {
+    title: "Animated Website",
+    tools: ["Sass", "React", "Firebase"],
+    link: "https://michiasshiferaw.github.io/home/",
+  },
+  {
+    title: "Personal Website",
+    tools: ["Bootstrap", "React", "Firebase"],
+    link: "https://porto22.netlify.app/",
+  },
+  {
+    title: "Face Your Food",
+    tools: ["Bootstrap", "React", "Firebase"],
+    link: "https://faceyourfood.netlify.app/",
+  },
+  {
+    title: "CarDoctor",
+    tools: ["Bootstrap", "React"],
+    link: "https://carrdoctor.netlify.app/",
+  },
+  {
+    title: "Animated Game",
+    tools: ["Vanillajs"],
+    link: "/code",
+  },
+  {
+    title: "Costco Management",
+    tools: ["C++"],
+    link: "/code",
+  },
+];
